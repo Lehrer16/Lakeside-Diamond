3bf31557aabb065905d0e02a73a6f1c8d462d518
Lehrer16 <jesseisthegreatest@gmail.com>
2025-01-14 01:44:10 -0600

    updated login form name

--- Lakeside/client/src/Pages/Login.tsx
+++ Lakeside/client/src/Pages/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../assets/logo.png';
 import Navbar from "../Components/Navbar";
-import LoginForm from '../Components/Login';
+import LoginForm from '../Components/Login Form';
 
 
 const Login: React.FC = () => {
